0595bc90da6789ed3b0c8fdebebd1d52c7795ccb
J. Fernando Sánchez <balkian@gmail.com>
2024-02-20 08:09:21 +0100

    clase 7

---
+++ src/es/upm/dit/adsw/g23/clases/tema01/Clase7/Ejercicios.java
@@ -0,0 +1,28 @@
+package es.upm.dit.adsw.g23.clases.tema01.Clase7;
+
+public class Ejercicios {
+	/**
+	 * Ordena el array dado usando BubbleSort en el sitio (in-place)
+	 * @param array
+	 */
+	static void burbuja(int[] array) {
+	}
+	/**
+	 * Ordena el array dado usando Selection Sort en el sitio (in-place)
+	 * @param array
+	 */
+	static void seleccion(int[] array) {
+	}
+	/**
+	 * Ordena el array dado usando Insertion Sort en el sitio (in-place)
+	 * @param array
+	 */
+	static void insercion(int[] array) {
+	}
+	/**
+	 * Ordena el array dado usando Merge Sort en el sitio (in-place)
+	 * @param array
+	 */
+	static void mergeSort(int[] array) {
+	}
+}
